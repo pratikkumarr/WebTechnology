b9804ab3e9d66aa9eb14781d3665ee8a29184e10
Pratik Kumar <pratiikkumarr@gmail.com>
2026-02-10 11:08:42 +0530

    Async

---
+++ asynchronous.js
@@ -0,0 +1,12 @@
+console.log("Starting my calculation")
+
+console.log("Start")
+function calculation(){
+    for(let a=0; a<1e10; a++){
+
+    }
+}
+
+
+calculation()
+console.log("Ending my Calculation")
