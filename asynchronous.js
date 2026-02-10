80f41aa9dafd987b7d2c9531bb6d2983f3cb0ecc
Pratik Kumar <pratiikkumarr@gmail.com>
2026-02-10 11:28:56 +0530

    asynch2

--- asynchronous.js
+++ asynchronous.js
@@ -1,12 +1,18 @@
-console.log("Starting my calculation")
+// console.log("Starting my calculation")
 
-console.log("Start")
-function calculation(){
-    for(let a=0; a<1e10; a++){
+// console.log("Start")
+// function calculation(){
+//     for(let a=0; a<1e10; a++){
 
-    }
-}
+//     }
+// }
 
+// calculation()
+// console.log("Ending my Calculation")
 
-calculation()
-console.log("Ending my Calculation")
+console.log("Hello1")
+setTimeout(function(){
+    console.log("Hello2");
+}, 2000);
+
+console.log("Hello3")
