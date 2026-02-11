8bb1aa0ec374d512404e91bc8d46260edb85adf0
Pratik Kumar <pratiikkumarr@gmail.com>
2026-02-11 23:54:33 +0530

    Promise

--- promise.js
+++ promise.js
@@ -40,22 +40,72 @@
 
 // ---------------------------------------------------------------------
 
+// const promiseOne = new Promise(function(resolve, reject){
+//     let error = true
+//     if(!error){
+//         resolve({username:"Ramesh", id: 123})
+//     } else {
+//         reject("Something went wrong")
+//     }
+// });
+
+// promiseOne.then(function(u1){
+//     console.log(u1)
+//     return u1.username
+// }).then((username)=>{
+//     console.log(username)
+// }).catch(function(err){
+//     console.log(err)
+// }).finally(()=>{
+//     console.log("All operations executed")
+// })
+
+
+// ------------------------------------------------------
+// Pratik Kumar
+// CS 4C
+// 2400290120187
+// const promiseOne = new Promise(function(resolve, reject){
+//     setTimeout(()=>{
+//         let error = true
+//         if(!error){
+//             resolve({username: "Ramesh", pass: 123})
+//         } else {
+//             reject("Something went wrong")
+//         }
+//     }, 3000)
+// })
+
+// async function consumepromiseOne(){
+//     try{
+//         const response = await promiseOne
+//         console.log(response)
+//     } catch(error){
+//         console.log(error)
+//     }
+// }
+// consumepromiseOne()
+
+// --------------------------------------------------------
+// Pratik Kumar
+// CS 4C
+// 2400290120187
+
 const promiseOne = new Promise(function(resolve, reject){
-    let error = true
-    if(!error){
-        resolve({username:"Ramesh", id: 123})
-    } else {
-        reject("Something went wrong")
-    }
+    setTimeout(() => {
+        let error = true;
+        
+        if(!error){
+            resolve("success");
+        } else {
+            reject("Something went wrong");
+        }
+    }, 2000);
 });
 
-promiseOne.then(function(u1){
-    console.log(u1)
-    return u1.username
-}).then((username)=>{
-    console.log(username)
-}).catch(function(err){
-    console.log(err)
-}).finally(()=>{
-    console.log("All operations executed")
-})
+promiseOne.then(function(message){
+        console.log("Success:", message);
+    })
+    .catch(function(error){
+        console.log("Error:", error);
+    });
