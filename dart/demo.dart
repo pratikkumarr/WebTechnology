cc1ae9e9534aa45e8f087ab9c382879914d5bf96
Pratik Kumar <pratiikkumarr@gmail.com>
2026-02-18 14:59:48 +0530

    input & basics

--- dart/demo.dart
+++ dart/demo.dart
@@ -1,5 +1,64 @@
-void main() {
-  Object a = "Hello";
-  //   a = 10;
-  print(a);
-}
+// void main() {
+//   String? name;
+//   //   a = 10;
+//   print(name.toString());
+//   print(name.hashCode);
+// }
+
+// ---------- Late Variable -----------------
+
+// late String name;
+
+// void main() {}
+
+// ------------------------------------------
+// void main() {
+//   final time = DateTime.now();
+//   print(time);
+// }
+
+// -------------------------------------------
+
+// void main() {
+// const list = [1, 2, 3, 4];
+// list.add(6);
+// print(list);
+
+// ----------------------
+
+// var hex = 0xABCDF;
+// print(hex);
+// print(hex.runtimeType);
+
+// ----------------------
+
+//   var v2 = 1.32e5;
+//   print(v2);
+//   print(v2.runtimeType);
+// }
+
+// --------------- CONVERSION --------------
+
+// void main() {
+//   // ----- string -> integer -------
+//   var one = int.parse('1');
+//   // assert only shows a message when the condition is false
+//   assert(one == 1);
+
+//   // ------- string -> double -----
+
+//   var onePointOne = double.parse('1.1');
+//   assert(onePointOne == 1.1);
+
+//   // ----- int -> string -------
+//   String One = 1.toString();
+//   assert(1 == "One");
+// }
+
+// --------- access using dollar $ ---------------
+
+// void main() {
+//   String fname = "Ramesh";
+//   String lname = "Kumar";
+//   print("$fname $lname");
+// }
